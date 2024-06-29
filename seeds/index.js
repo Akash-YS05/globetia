ee3cc60c1fc67b4b628c423a0cb4acacb9fc06a9
Akash-YS05 <akash.pan05@gmail.com>
2024-06-29 17:02:14 +0530

    added error handling with joi

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const seedDb = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            image: 'https://unsplash.com/collections/SAoQrQh_hg8/landscape-wallpaper',
             description: '  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi libero temporibus nam aspernatur ea! Doloribus, labore placeat excepturi, sit aliquam libero esse quisquam minus tenetur consequatur eius deleniti, sed perspiciatis.',
             price
         })
@@ -32,3 +32,4 @@ const seedDb = async () => {
 seedDb().then(() => {
     mongoose.connection.close()
 })
+
